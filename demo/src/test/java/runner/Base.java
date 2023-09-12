dc40e01f42b903e072890706ee6daed8f5ba3d6f
Iamneo <techadmin@iamneo.ai>
2023-09-12 10:43:49 +0000

    Backup Commit

--- demo/src/test/java/runner/Base.java
+++ demo/src/test/java/runner/Base.java
@@ -35,7 +35,7 @@ public class Base {
 
 		if(browserName.equalsIgnoreCase("chrome")) {			
 			try {
-				driver = new RemoteWebDriver(new URL("http://34.85.201.58:4450"), capabilities);
+				driver = new RemoteWebDriver(new URL("http://localhost:4444"), capabilities);
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
